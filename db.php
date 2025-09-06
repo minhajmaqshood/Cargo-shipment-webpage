68f0cf3f9b73693d76e8a0ca4c7fe921d4ceafaf
minhajmaqshood <maqshood14212@gmail.com>
2025-09-06 13:25:28 +0530

    Update db.php
    Signed-off-by: minhajmaqshood <maqshood14212@gmail.com>

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$dbname = "shipment_db";
+$dbname = "Your_backend_database";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
